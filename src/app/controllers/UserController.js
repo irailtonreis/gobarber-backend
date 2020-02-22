7fb21a25a047e84a8a7543f2e61dced1d05d33ba
irailtonreis <irailton@gmail.com>
2020-02-22 11:50:25 -0300

    finalizando método store UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,15 +2,20 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const { email } = req.body.email;
-    console.log(email);
-    // const userExists = await User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    // if (userExists) {
-    //   return res.status(400).json({ error: 'User already exists.' });
-    // }
-    const user = await User.create(req.body);
-    return res.json(user);
+    if (userExists) {
+      return res.status(400).json({ error: 'User alreaddy exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
