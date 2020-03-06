25cf1c6d8ce96053ffd1696f4cefd237d92c6d06
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-06 12:11:09 -0300

    Adicionando cancelamento appointments e envio email provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,11 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-
+import Mail from '../../lib/Mail';
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -104,6 +104,32 @@ class AppointmentController {
       user: provider_id,
     });
     return res.json(appointmentt);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't permission to cancel this appointment",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    appointment.save();
+
+    await Mail.sendMail({
+      to: ''
+    })
+
+    return res.json(appointment);
   }
 }
 
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '111de278783d13',
+    pass: 'baf0593858a0d5',
+  },
+  default: {
+    from: 'Equipe Gobarber<noreply@gobarber.com>',
+  },
+};
