a2932aa15d1a921a021d4d5865f1376075452254
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-07 16:30:21 -0300

    Configurações Queue

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,4 +1,8 @@
 import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
 
 class Queue {
   constructor() {
@@ -7,7 +11,25 @@ class Queue {
   }
 
   init() {
-    
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+      bee.process(handle);
+    });
   }
 }
 
